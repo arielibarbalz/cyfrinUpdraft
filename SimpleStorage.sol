ed52f8d8d6197236704d7b8fa7b5515005d79983
arielibarbalz <ibarbalz@gmail.com>
2026-08-05 23:19:49 -0300

    Inheritance in Solidity,

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -17,7 +17,7 @@ contract SimpleStorage {
 
     mapping (string => uint256) public nameToFavoriteNumber;
  
-    function store(uint256 _favoriteNumber) public{
+    function store(uint256 _favoriteNumber) public virtual {
         myFavoriteNumber = _favoriteNumber;
     }
     function retrieve() public view returns (uint256){
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -11,8 +11,11 @@ contract StorageFactory {
         listOfSimpleStorageContracts.push(simpleStorageContractVariable);
     }
     
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {}
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 
 }
